6a0a7fca00b9d46a1747d41f01ae0882352c1228
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-10-17 18:25:28 -0400

    continue server setup

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const util = require("util");
 // Asynch Processes
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+// Setting up Server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
